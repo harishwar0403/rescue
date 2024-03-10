cf578e431be900e96411b9f51ac07b8ec2dc6daa
harishwar0403 <harishwars@anjaconline.org>
2024-03-10 14:30:00 +0530

    api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-export const authUrl = "http://localhost:4000/auth";
-export const notificationUrl = "http://localhost:4000/notification";
-// export const authUrl = "https://flood-prevention-ty6q.vercel.app/auth";
-// export const notificationUrl = "https://flood-prevention-ty6q.vercel.app/notification";
+// export const authUrl = "http://localhost:4000/auth";
+// export const notificationUrl = "http://localhost:4000/notification";
+export const authUrl = "https://flood-prevention-ty6q.vercel.app/auth";
+export const notificationUrl = "https://flood-prevention-ty6q.vercel.app/notification";
