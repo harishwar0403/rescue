3b9fe2c83b0f60feb7c87025cce7f7fa153295b5
harishwar0403 <harishwars@anjaconline.org>
2024-03-10 18:30:06 +0530

    api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 // export const authUrl = "http://localhost:4000/auth";
 // export const notificationUrl = "http://localhost:4000/notification";
-export const authUrl = "https://rescue-virid.vercel.app/auth";
+export const authUrl = "https://rescue-api.vercel.app/auth";
 export const notificationUrl =
-  "https://rescue-virid.vercel.app/notification";
+  "https://rescue-api.vercel.app/notification";
